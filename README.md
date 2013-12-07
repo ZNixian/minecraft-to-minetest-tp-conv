dc3a81d4ed5204900264bb33fafdb90bb5166fa0
minermoder27 <minermoder27@gmail.com>
2013-12-07 18:41:05 +1300

    Add code and readme

--- README.md
+++ README.md
@@ -2,3 +2,17 @@ minecraft-to-minetest-tp-conv
 =============================
 
 Minecraft texture pack to minetest texture pack converter
+
+
+To use:
+
+1. Get minecraft texture pack.
+2. Get "assets" folder from minecraft texture pack.
+3. Move "mc-to-mt-tp-conv" folder to folder with assets folder
+4. Run "conv.sh" (it is inside "mc-to-mt-tp-conv" folder)
+5. The folder "minetest-texture-pack" will be made.
+6. "minetest-texture-pack" is the converted texture pack.
+
+Enjoy!
+
+Note: works on mac/linux systems
---
+++ mc-to-mt-tp-conv/conv.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+cd ..
+rm -rf textures
+rm -rf uctextures
+mkdir textures
+mkdir uctextures
+cd uctextures
+mkdir b
+mkdir i
+cd ..
+
+
+FILES=$(dirname "$0")/../assets/minecraft/textures/blocks/*.png
+for f in $FILES
+do
+	cp $f "uctextures/b/"
+done
+
+FILES=$(dirname "$0")/../assets/minecraft/textures/items/*.png
+for f in $FILES
+do
+	cp $f "uctextures/i/"
+done
+
+cd uctextures
+
+while read p; do
+	if [ -n "$p" ]
+	then
+		if [ -e "trans.png" ]
+		then
+			mv trans.png "../textures/$p"
+		else
+			mv "$p" trans.png
+		fi
+	fi
+done < ../mc-to-mt-tp-conv/transtable.txt
+
+
+#####################################
+convert "b/destroy_stage_0.png" \
+	"b/destroy_stage_1.png" \
+	"b/destroy_stage_2.png" \
+	"b/destroy_stage_3.png" \
+	"b/destroy_stage_4.png" \
+	"b/destroy_stage_5.png" \
+	"b/destroy_stage_6.png" \
+	"b/destroy_stage_7.png" \
+	"b/destroy_stage_8.png" \
+	"b/destroy_stage_9.png" \
+			+append "../textures/crack-anylength.png"
+#####################################
+
+cd ..
+
+mv textures minetest-texture-pack
